35e9ee21a561b8036f480185dd9ee9bcf07b77e7
ksan <aquam41@gmail.com>
2024-12-05 10:54:14 +0900

    [Fix] Circle 도메인 서비스 수정

--- src/main/java/com/ddd/basic/domain/model/service/CircleService.java
+++ src/main/java/com/ddd/basic/domain/model/service/CircleService.java
@@ -5,8 +5,6 @@ import com.ddd.basic.domain.model.circle.ICircleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
-
 @RequiredArgsConstructor
 @Service
 public class CircleService {
@@ -14,12 +12,6 @@ public class CircleService {
     private final ICircleRepository circleRepository;
 
     public boolean exists(Circle circle) {
-        Circle foundCircle = circleRepository.find(circle.getName());
-        return Objects.nonNull(foundCircle);
-    }
-
-    private boolean isDuplicated(String circleName) {
-        Circle foundCircle = circleRepository.find(circleName);
-        return Objects.nonNull(foundCircle);
+        return circleRepository.find(circle.getName()).isPresent();
     }
 }
