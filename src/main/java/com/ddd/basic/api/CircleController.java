d06cfb0d4aa5fd7e68400bd98df2b0e584df3ce0
ksan <aquam41@gmail.com>
2024-12-05 10:48:41 +0900

    [Feat] 서클가입, 초대 api 작성

--- src/main/java/com/ddd/basic/api/CircleController.java
+++ src/main/java/com/ddd/basic/api/CircleController.java
@@ -2,6 +2,8 @@ package com.ddd.basic.api;
 
 import com.ddd.basic.application.circle.CircleApplicationService;
 import com.ddd.basic.application.circle.CircleCreateDto;
+import com.ddd.basic.application.circle.CircleInviteDto;
+import com.ddd.basic.application.circle.CircleJoinDto;
 import com.ddd.basic.common.ResponseModel;
 import com.ddd.basic.common.ResultMessage;
 import com.ddd.basic.common.constants.ExceptionMessage;
@@ -32,6 +34,38 @@ public class CircleController {
         } catch(IllegalArgumentException e) {
             log.error(ExceptionMessage.DUPLICATED_CIRCLE_NAME.getMessage(), e);
             res.error(ExceptionMessage.DUPLICATED_CIRCLE_NAME);
+        }
+        return res;
+    }
+
+    @PostMapping("join")
+    public ResponseModel join(@RequestBody CircleJoinDto joinInfo) {
+        ResponseModel res = new ResultMessage<>();
+        try {
+            circleApplicationService.join(joinInfo);
+            res.success();
+        } catch(NullPointerException e) {
+            log.error(ExceptionMessage.NOT_FOUND_CIRCLE.getMessage(), e);
+            res.error(ExceptionMessage.NOT_FOUND_CIRCLE);
+        } catch(NegativeArraySizeException e) {
+            log.error(ExceptionMessage.FULL_CIRCLE_MEMBERS.getMessage(), e);
+            res.error(ExceptionMessage.FULL_CIRCLE_MEMBERS);
+        }
+        return res;
+    }
+
+    @PostMapping("invite")
+    public ResponseModel invite(@RequestBody CircleInviteDto inviteInfo) {
+        ResponseModel res = new ResultMessage<>();
+        try {
+            circleApplicationService.invite(inviteInfo);
+            res.success();
+        } catch(NullPointerException e) {
+            log.error(e.getMessage(), e);
+            res.error(ExceptionMessage.valueOf(e.getMessage()));
+        } catch(NegativeArraySizeException e) {
+            log.error(ExceptionMessage.FULL_CIRCLE_MEMBERS.getMessage(), e);
+            res.error(ExceptionMessage.FULL_CIRCLE_MEMBERS);
         }
         return res;
     }
