4a9710e5d32bde85eaf07489e518e44766f94f49
ksan <aquam41@gmail.com>
2024-12-16 20:11:55 +0900

    [Feat] Configuration 이름 변경 및 WebSecurity 관련 설정 임의 추가

---
+++ src/main/java/com/ddd/basic/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ddd.basic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
