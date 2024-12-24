6eb8431e46543a4637610f3a7a67303db239d803
ksan <aquam41@gmail.com>
2024-12-24 16:37:47 +0900

    [Refactor] Jpa Auditing 설정 및 적용

--- src/main/java/com/ddd/basic/DddApplication.java
+++ src/main/java/com/ddd/basic/DddApplication.java
@@ -2,7 +2,9 @@ package com.ddd.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DddApplication {
 
--- src/main/java/com/ddd/basic/domain/shared/BasicEntity.java
+++ src/main/java/com/ddd/basic/domain/shared/BasicEntity.java
@@ -1,20 +1,24 @@
 package com.ddd.basic.domain.shared;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
 public class BasicEntity {
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createDate;
 
-    private LocalDateTime deleteDate;
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
 
-    protected void setCreateDateNow() {
-        this.createDate = LocalDateTime.now();
-    }
+    private LocalDateTime deleteDate;
 
     protected void setDeleteDateNow() {
         this.deleteDate = LocalDateTime.now();
